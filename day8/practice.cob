c89ff05c4d8b642252ed8c22ac8701bd1f13e236
nithinshashank007 <thort1336@gmail.com>
2025-09-26 23:31:50 -0500

    .

---
+++ day8/practice.cob
@@ -0,0 +1,11 @@
+identification division.
+program-id - table.
+data division.
+working storage section.
+01 num pic 9(5).
+01 num pic 9(5).
+01 result pic 9(5).
+procedure division.
+display "455".
+move 455 to num 
+stop run.
